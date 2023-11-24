fb98c230e52fd008a2086489f711d7528f69452c
hadriengithub <75088711+hadriengithub@users.noreply.github.com>
2023-11-24 17:49:25 +0100

    Update XorEncrypt.py
    This is an update to this Xor script

--- XorEncrypt.py
+++ XorEncrypt.py
@@ -1,10 +1,13 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="default")
-parser.add_argument('data', type=str, help="initial data to encrypt")
-parser.add_argument('key', type=int, help="key to encrypt the data")
+parser=argparse.ArgumentParser(description="default")
+parser.add_argument('given',type=str,help="This is the data to be encoded")
+parser.add_argument('key',type=int,help="This is the key to encrypt the given data")
+args=parser.parse_args()
+given=args.given
+key=args.key
 
-args = parser.parse_args()
-
-print("initial data : ", args.data)
-print("key : ", args.key)
+print("crypto{",end="")
+for x in given:
+	print(chr(ord(x)^key),end="")
+print("}")
